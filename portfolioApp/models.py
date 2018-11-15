37b5064dd299d5a32a58ad9efeb86f69bade07c3
Lotus <cmadisonne@gmail.com>
2018-11-15 11:02:40 -0600

    Updated

--- portfolioApp/models.py
+++ portfolioApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ContactModal(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    message = models.CharField(max_length=1000)
+    message = models.TextField(max_length=500)
 
     def __str__(self):
         return self.name
